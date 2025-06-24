62dc6adb7ca0267d71db42f81e906931cf02cae7
Taha Yaseen <thetahayaseenu@gmail.com>
2025-06-24 19:17:45 +0500

    (Convert) ByPascalCase Method Added

--- lib/utils/extensions/enum_extensions.dart
+++ lib/utils/extensions/enum_extensions.dart
@@ -4,5 +4,12 @@ extension EnumUtils<T extends Enum> on List<T> {
 
   List<String> get names => map((value) => value.name).toList(); 
   List<String> get pascalCaseNames => map((value) => value.name.toPascalCase()).toList(); 
+
+  T byPascalCase(String pascalName) {
+    return firstWhere(
+      (e) => e.name.toPascalCase() == pascalName,
+      orElse: () => throw ArgumentError('No enum value with PascalCase name "$pascalName" found.'),
+    );
+  }
 } 
 
